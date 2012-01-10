22fda189a809956694f59fd16eb5782d128b12e5
Donald Clark Jackson <dcj@clark-communications.com>
2012-01-10 10:43:13 -0800

    Added doc strings

--- util-convert/src/util/convert.clj
+++ util-convert/src/util/convert.clj
@@ -3,11 +3,13 @@
   (:import [java.util UUID]))
 
 (defn string->uuid
+  "Convert a string to a UUID"
   [stringified-uuid]
   {:pre [(string? stringified-uuid)]}
   (. UUID (fromString stringified-uuid)))
 
 (defn string->float
+  "Convert a string to a Float"
   [stringified-float]
   {:pre [(string? stringified-float)]}
   (Float/parseFloat stringified-float))
