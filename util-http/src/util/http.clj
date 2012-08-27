4480e79a84b812b45052d8cc35b9b0b0ea1c7e89
Donald Clark Jackson <dcj@clark-communications.com>
2012-08-27 16:28:48 -0700

    Adding header time functions

--- util-http/src/util/http.clj
+++ util-http/src/util/http.clj
@@ -1,6 +1,7 @@
 (ns util.http
   "Utilities for dealing with HTTP"
-  (:use [slingshot.slingshot :only [throw+ try+]]))
+  (:use [slingshot.slingshot :only [throw+ try+]])
+  (:import  [org.apache.http.impl.cookie DateUtils DateParseException]))
 
 (defn status-range
   "Returns most significant digit of http-response-code, assumes 3 digit response codes"
@@ -16,6 +17,24 @@
   "Predicate that attempts to verify that the given response map came from HTTP.  This is a total hack!"
   [hrm]
   (get hrm :status false))
+
+(defn parse-header-date
+  [date-string]
+  (. DateUtils (parseDate date-string)))
+
+(defn filter-header-times
+  [header-map]
+  (select-keys header-map [:date :expires :last-modified]))
+
+(defn parse-header-times
+  "The idea here is to catch the DateParseException, return nil, and move on....."
+  [header-map]
+  (apply merge
+	 (for [[k v] (filter-header-times header-map)]
+	   (try+
+	    {k (parse-header-date v)}
+	    (catch DateParseException _
+	      nil)))))
 
 
 
